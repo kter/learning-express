0000fe310a4183f31c69eee5d3b74ad513293cf1
kter <me@kter.jp>
2018-01-28 08:25:27 +0000

    show messages

--- routes/boards.js
+++ routes/boards.js
@@ -5,11 +5,15 @@ var connection = require('../mysqlConnection');
 
 router.get('/:board_id', function(req, res, next) {
     var boardId = req.params.board_id;
-    var query = 'SELECT * FROM boards WHERE board_id = ' + boardId;
-    connection.query(query, function(err, board) {
-        res.render('board', {
-            title: board[0].title,
-            board: board[0]
+    var getBoardQuery = 'SELECT * FROM boards WHERE board_id = ' + boardId;
+    var getMessagesQuery = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages WHERE board_id = ' + boardId;
+    connection.query(getBoardQuery, function(err, board) {
+        connection.query(getMessagesQuery, function(err, messages) {
+            res.render('board', {
+                title: board[0].title,
+                board: board[0],
+                messageList: messages
+            });
         });
     });
 });
