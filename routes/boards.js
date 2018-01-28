fa89fc6f3bd12a741bf50d2d40abffbf470b4f21
kter <me@kter.jp>
2018-01-28 08:11:31 +0000

    post message

--- routes/boards.js
+++ routes/boards.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 var connection = require('../mysqlConnection');
 
 router.get('/:board_id', function(req, res, next) {
@@ -10,6 +11,17 @@ router.get('/:board_id', function(req, res, next) {
             title: board[0].title,
             board: board[0]
         });
+    });
+});
+
+router.post('/:board_id', function(req, res, next) {
+    var message = req.body.message;
+    var boardId = req.params.board_id;
+    var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+    var query = 'INSERT INTO messages (message, board_id, created_at) VALUES ("' + message + '", ' + '"' + boardId + '", ' + '"' + createdAt + '")';
+    connection.query(query, function(err, rows) {
+        console.log(err);
+        res.redirect('/boards/' + boardId);
     });
 });
 
