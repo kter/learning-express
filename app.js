ee9430f01d4e26095c38185a00d487b134e38556
kter <me@kter.jp>
2018-01-29 23:18:35 +0000

    logout function

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 var boards = require('./routes/boards');
 var register = require('./routes/register');
 var login = require('./routes/login');
+var logout = require('./routes/logout');
 
 var setUser = require('./setUser');
 
@@ -40,6 +41,7 @@ app.use('/users', users);
 app.use('/boards', boards);
 app.use('/register', register);
 app.use('/login', login);
+app.use('/logout', logout);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
