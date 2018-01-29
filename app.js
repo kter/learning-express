db7446fe8872f1bb33d423c74868d7f939fe9af8
kter <me@kter.jp>
2018-01-29 07:38:23 +0000

    register / login / display account

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var boards = require('./routes/boards');
+var register = require('./routes/register');
+var login = require('./routes/login');
+
+var setUser = require('./setUser');
 
 var app = express();
 
@@ -22,10 +27,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'cyjjymanmtzx',
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.use('/', index);
+app.use('/', setUser);
+app.use('/boards', setUser);
+app.use('/',  index);
 app.use('/users', users);
 app.use('/boards', boards);
+app.use('/register', register);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
